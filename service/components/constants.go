bb93a9b8ef3ed373f61a2ab9ddc9755ed034ae95
Christian <104896934+christian-gennarelli@users.noreply.github.com>
2023-12-04 22:31:03 +0100

    To-do appendix added.

--- service/components/constants.go
+++ service/components/constants.go
@@ -1,3 +1,3 @@
 package components
 
-const USERNAME_REGEXP = "/^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){6,14}[a-zA-Z0-9]$/"
+const USERNAME_REGEXP = "/^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){6,14}[a-zA-Z0-9]$/" // TO BE MODIFIED: SEARCH FOR REGEXPS SPECIFIC FOR GOLANG
